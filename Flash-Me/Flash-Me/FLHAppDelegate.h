344119af5bc930f029f6dcac521566de9d7cfb18
Keith Miller <keithmil@andrew.cmu.edu>
2013-12-01 22:15:10 -0500

    new starting repo

---
+++ Flash-Me/Flash-Me/FLHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLHAppDelegate.h
+//  Flash-Me
+//
+//  Created by Keith Miller on 12/1/13.
+//  Copyright (c) 2013 donald.keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
